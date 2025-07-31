aee6225ea71c75509f312be737ff863cbc0a4ab6
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-07-31 11:19:16 +0900

    ;done lec'

--- js-and-dom/javascript/application.js
+++ js-and-dom/javascript/application.js
@@ -1,1 +1,21 @@
-console.log("Hello from JavaScript!");
+console.log("Hello from js!");
+
+const players = document.querySelector("#players");
+
+// use js to insert elements
+// players.insertAdjacentHTML("beforeend", "<li>Messi</li>");
+// players.insertAdjacentHTML("beforeend", "<li>Neymar</li>");
+
+const messi = players.querySelector(".red");
+console.log(messi.innerText);
+
+// selecting multiple elements
+
+const countries = document.querySelectorAll("#fifa-wins li");
+console.log(countries);
+
+// Insert france to the list
+// select the ordered list
+const fifaWins = document.querySelector("#fifa-wins");
+// insert the element that says France (2 wins)
+fifaWins.insertAdjacentHTML("beforeend", "<li>France (2 wins)</li>");
